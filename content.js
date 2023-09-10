862dc743902e1bcccf4377f4d03b239949df8cce
Simar Kaur <89771491+simarkaur28@users.noreply.github.com>
2023-09-11 02:53:40 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,40 +1,40 @@
 // content.js
-console.log("hello jasmit ");
-
-// Define a function to hide images
-function hideImages() {
-  const images = document.querySelectorAll('img'); // Select all <img> elements
-
-  images.forEach((image) => {
-    image.style.display = 'none'; // Hide the image by setting its display property to 'none'
-  });
-}
-
-
-// Call the hideImages function immediately when the content script runs
-hideImages();
-
-
+console.log("Welcome to SafeBrowse ");
 // Get all the image elements on the page
 const imageElements = document.getElementsByTagName("img");
 
 // Create an array to store the image URLs
 const imageUrls = [];
 
+
+
 // Loop through the image elements and extract their src attributes
 for (let i = 0; i < imageElements.length; i++) {
   const imageUrl = imageElements[i].src;
+
   imageUrls.push(imageUrl);
 }
-// fetch()
 
 // Now you can do whatever you want with the extracted image URLs
 console.log(imageUrls);
-
-// Create a JSON object containing the image URLs
-const data = { imageUrls };
-
-// Send a POST request to your Flask server
-
 // content.js
 
+// Make an HTTP POST request to the /generate_caption route
+for (let i = 0; i < imageUrls.length; i++) {
+  fetch('http://127.0.0.1:5000/generate_caption', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ image_url: imageUrls[i] }) // Send the current image URL
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Response for image " + i + ":");
+      console.log('Response from /generate_caption:', data);
+      // You can access the generated caption in data.caption and print it
+    })
+    .catch(error => {
+      console.error('Error for image ' + i + ':', error);
+    });
+}
