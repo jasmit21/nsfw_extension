eceadb749a293efeccc2c7ac4b2a6a32e0770578
Jasmit Rathod <89774786+jasmit21@users.noreply.github.com>
2023-10-30 22:33:30 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,68 +1,88 @@
 // Function to extract image URLs from the page
-console.log("hoga ??");
+console.log("Welcome To SAFEBROWSE's console");
 function extractImageUrls() {
   const images = Array.from(document.querySelectorAll("img"));
   return images.map((img) => img.src);
 }
 
 // Function to hide all images on the page
-function hideImageByUrl(imageUrl) {
+function hideImages() {
   const images = document.querySelectorAll("img");
   images.forEach((img) => {
-    if (img.src === imageUrl) {
-      img.style.display = "none";
-    }
+    img.style.display = "none";
   });
 }
 
 // Function to show images based on the server response
+
 function showImages(imageUrls) {
-  const images = document.querySelectorAll("img");
-  images.forEach((img, index) => {
-    if (imageUrls[index]) {
-      img.src = imageUrls[index];
+  console.log("inside Show images function\n");
+  if (typeof imageUrls === "string") {
+    // If a single URL is provided, find the matching img element
+    const img = document.querySelector(`img[src="${imageUrls}"]`);
+    if (img) {
+      img.src = imageUrls;
       img.style.display = "block";
     }
-  });
+  } else if (Array.isArray(imageUrls)) {
+    // If an array of URLs is provided, update images in their original positions
+    imageUrls.forEach((imageUrl, index) => {
+      const img = document.querySelector(`img[src="${imageUrl}"]`);
+      if (img) {
+        img.src = imageUrl;
+        img.style.display = "block";
+      }
+    });
+  }
 }
+
 
 // Extract image URLs from the page and hide the images
 const imageUrls = extractImageUrls();
-// hideImages();
+hideImages();
 console.log(imageUrls);
+const allowedImageUrls = [];
+// Make an HTTP POST request to the server to fetch responses
+fetch("http://127.0.0.1:5000/generate_caption", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ image_urls: imageUrls }),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Response from server:", data);
 
-// Make an HTTP POST request to the /generate_caption route
-for (let i = 0; i < imageUrls.length; i++) {
-  fetch('http://127.0.0.1:5000/generate_caption', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ image_url: imageUrls[i] }) // Send the current image URL
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Response for image " + i + ":");
-      console.log('Response from /generate_caption:', data);
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].label == "Sexually explicit" || data[i].label == "Violent") {
+        if (data[i].scores < 0.6) {
+          console.log(
+            "Label for Image ",
+            i + 1,
+            ":",
+            data[i].label,
+            "img_url: ",
+            data[i].image_url
+          );
 
-      // Find the category with the highest score
-      let maxScore = -1;
-      let maxCategory = '';
-      for (let j = 0; j < data.labels.length; j++) {
-        if (data.scores[j] > maxScore) {
-            maxScore = data.scores[j];
-            maxCategory = data.labels[j];
-            }
-          }
-      
-      console.log('Highest scoring category for image ' + i + ':', maxCategory, 'with score:', maxScore);
-      if(maxCategory == 'Sexually explicit'){
-        hideImageByUrl(imageUrls[i]);
+          allowedImageUrls.push(data[i].image_url);
+        }
+      } else if (data[i].label == "Neutral") {
+        console.log(
+          "Label for Image ",
+          i + 1,
+          ":",
+          data[i].label,
+          "img_url: ",
+          data[i].image_url
+        );
+
+        allowedImageUrls.push(data[i].image_url);
       }
-      
-      // You can access the generated caption in data.caption and print it
-    })
-    .catch(error => {
-      console.error('Error for image ' + i + ':', error);
-    });
-}
+    }
+    showImages(allowedImageUrls);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
